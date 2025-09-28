071028fd33109c79e6b88a6021188eac405a653c
nik03072005 <nc03072005@gmail.com>
2025-09-29 02:43:37 +0530

    Fix hostname in remotePatterns for image configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
       },
        {
         protocol: 'https',
-        hostname: 'duramacollege.s3.ap-south-1.amazonaws.com',
+        hostname: 'mangaldaicollege.s3.ap-south-1.amazonaws.com',
         port: '',
         pathname: '/**',
       },
